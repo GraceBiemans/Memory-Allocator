0fc5d827ed310c934f7d887225c5d72d2f61ff9f
GraceBiemans <97420629+GraceBiemans@users.noreply.github.com>
2022-01-10 15:28:53 -0600

    Create free.h

---
+++ free.h
@@ -0,0 +1,9 @@
+// Grace Biemans & Nick Heleta
+// geb965 & nwh397
+
+/**
+ * Function to free the given chunk
+ * @param *p: the chunk to free
+ * @return: 0 on success and -1 for failure
+ */
+int M_Free(void *p);
