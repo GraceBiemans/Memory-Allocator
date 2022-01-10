8782757b6ca493706d7bd6209611d1581fb9528a
GraceBiemans <97420629+GraceBiemans@users.noreply.github.com>
2022-01-10 15:26:45 -0600

    Create alloc.h

---
+++ alloc.h
@@ -0,0 +1,12 @@
+// Grace Biemans & Nick Heleta
+// geb965 & nwh397
+
+#define magic 1234567;
+
+/**
+ * Function to allocate a chunk
+ * @param size: the requested size in bytes
+ * @return: a pointer to the start of the allocated chunk, or NULL if the request fails
+ */
+void *M_Alloc(int size);
+
